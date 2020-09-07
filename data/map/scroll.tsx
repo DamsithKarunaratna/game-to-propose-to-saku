9c7e6887c3fdd19f2919dfc763524504cf4008ac
Damsith Karunaratna <dkarunarat@wiley.com>
2020-09-08 01:40:16 +0530

    Save completed game in repository

---
+++ data/map/scroll.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="scroll" tilewidth="35" tileheight="35" tilecount="204" columns="17">
+ <image source="../img/scroll.png" width="600" height="442"/>
+</tileset>
